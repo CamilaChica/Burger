de80e5c898cabbbe9514fbf4b8c58b848300184e
Camila Chica <50186503+CamilaChica@users.noreply.github.com>
2019-09-22 21:14:49 -0400

    schema fixed

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 id int not null auto_increment,
 burger_name VARCHAR(200) NOT NULL,
-devoured boolean NOT NULL,
+devoured boolean NOT NULL default 0,
 created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (ID)
