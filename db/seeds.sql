a56bc6b974e8bb4e06a2e8f0df8b2bb172b0e40f
Camila Chica <50186503+CamilaChica@users.noreply.github.com>
2019-09-22 21:24:27 -0400

    seeds established

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("IMPOSSIBLE WHOPPERER");
+INSERT INTO burgers (burger_name) VALUES ("BIG BUILDING");
+INSERT INTO burgers (burger_name) VALUES ("MINI BURGER");
+INSERT INTO burgers (burger_name) VALUES ("VEGAN BURGER");
+INSERT INTO burgers (burger_name) VALUES ("DOUBLE BURGER");
