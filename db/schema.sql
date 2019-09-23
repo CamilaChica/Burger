7751dc824b05a3077e25afed5f9051b1ad29d0f0
Camila Chica <50186503+CamilaChica@users.noreply.github.com>
2019-09-22 21:12:47 -0400

    Database and table created and copied in schema

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id int not null auto_increment,
+burger_name VARCHAR(200) NOT NULL,
+devoured boolean NOT NULL,
+created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (ID)
+);
